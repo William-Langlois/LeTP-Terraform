67a10a1a58486a49d9c50f48ea4f94d9b8ebfec0
william Langlois <wlanglois@n-ine.fr>
2023-03-23 16:53:54 +0100

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
-var app = express();app.listen(443, () => {
- console.log("Server running on port 443");
+var app = express();app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
 
 const phones = ["Iphone","Alcatel"]
