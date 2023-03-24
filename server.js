8eaf6d5a4e23ac3c604ce4ace530a7cb6ade1ddf
william Langlois <wlanglois@n-ine.fr>
2023-03-24 15:40:04 +0100

    fix API

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require("express");
 const bodyParser = require('body-parser')
-const { Client } = require('pg')
 
 const queries = require("./queries")
 
-
 var app = express();
 app.use(bodyParser.json())
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -17,26 +16,24 @@ app.listen(process.env.PORT, () => {
  console.log("Server running on port",process.env.PORT);
 });
 
-const phones = ["Iphone","Alcatel"]
-//TODO : turn into postgres
 app.get("/get-phones", async(req, res, next) => {
     try{
         var result =  await queries.getPhones(req,res);
-        res.status(200,result)
+        res.status(200).json(result);
     }
     catch(ex)
     {
-        res.status(500,ex);
+        res.status(500).json(ex);
     }
 });
 
 app.post("/add-phone", async (req, res, next) => {
     try{
         var result =  await queries.addPhone(req,res);
-        res.status(200,result)
+        res.status(200).json(result);
     }
     catch(ex)
     {
-        res.status(500,ex);
+        res.status(500).json(ex);
     }
 });
